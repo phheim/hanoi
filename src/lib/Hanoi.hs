d55e9f063793414260aec53e4da10c7b2843c8bd
Philippe Heim <Heim@ProjectJARVIS.de>
2020-05-07 09:48:09 +0200

    encapsulated everything in the library

--- src/lib/Hanoi.hs
+++ src/lib/Hanoi.hs
@@ -1,2 +1,49 @@
--- THIS IS A DUMMY
-module Hanoi where
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Hanoi
+-- Maintainer  :  Philippe Heim (Heim@ProjectJARVIS.de)
+--                Gideon Geier (geier@projectjarvis.de)
+--
+-- This module exports all stuff of the hanoi library
+--
+-----------------------------------------------------------------------------
+module Hanoi
+  ( AP
+  , AcceptanceCondition
+  , AcceptanceSet
+  , AcceptanceType
+  , HOA(..)
+  , HOAAcceptanceName(..)
+  , HOAProperty(..)
+  , Label
+  , Formula
+  , State
+  -- Parsing
+  , hoaParser
+  -- Printing
+  , printHOA
+  , printHOALines
+  -- Sanitizing
+  , complete
+  , sanitize
+  ) where
+
+import HOA.Format
+  ( AP
+  , AcceptanceCondition
+  , AcceptanceSet
+  , AcceptanceType
+  , HOA(..)
+  , HOAAcceptanceName(..)
+  , HOAProperty(..)
+  , Label
+  , State
+  )
+
+import Sat.Finite (Formula)
+
+import HOA.Printer (printHOA, printHOALines)
+
+import HOA.Parser (hoaParser)
+
+import HOA.Sanitizer (complete, sanitize)
