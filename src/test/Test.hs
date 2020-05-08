d1cc2865798f562cc8fd9ca24558904ec214e331
Philippe Heim <heim@projectjarvis.de>
2020-05-08 07:23:58 +0000

    Spot based testing

---
+++ src/test/Test.hs
@@ -0,0 +1,41 @@
+----------------------------------------------------------------------------
+-- |
+-- Module      :  Test
+-- Maintainer  :  Philippe Heim
+--
+-- Standard TestSuite.
+--
+-----------------------------------------------------------------------------
+{-# LANGUAGE LambdaCase, RecordWildCards #-}
+
+-----------------------------------------------------------------------------
+module Test
+  ( Test.tests
+  ) where
+
+-----------------------------------------------------------------------------
+import Distribution.TestSuite
+
+import SpotBasedTest (tests)
+
+-----------------------------------------------------------------------------
+-- | The Tests
+tests :: IO [Test]
+tests = do
+  dummyTest <- dummy
+  spotTests <- SpotBasedTest.tests
+  return $ map Test $ [dummyTest] ++ spotTests
+
+-----------------------------------------------------------------------------
+-- | This is the dummy test
+dummy :: IO TestInstance
+dummy =
+  let dum =
+        TestInstance
+          { run = return $ Finished Pass
+          , name = "dummy"
+          , tags = []
+          , options = []
+          , setOption = \_ _ -> Right dum
+          }
+   in return dum
