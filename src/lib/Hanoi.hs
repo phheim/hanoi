2e21453a875a19258ca8034ebe63b333d31455ac
Marvin Stenger <stenger@projectjarvis.de>
2020-05-22 14:12:51 +0200

    add more checkValidHOA method + Autfilt wrapper

--- src/lib/Hanoi.hs
+++ src/lib/Hanoi.hs
@@ -27,6 +27,8 @@ module Hanoi
   -- Sanitizing
   , complete
   , sanitize
+  -- Checking
+  , checkValidHOA
   ) where
 
 import HOA.Format
@@ -48,3 +50,48 @@ import HOA.Printer (printHOA, printHOALines)
 import HOA.Parser (hoaParser, parse)
 
 import HOA.Sanitizer (complete, sanitize)
+
+import Spot.Autfilt
+  ( autfilt
+  , AutfiltInput(automaton)
+  , defaultAutfiltInput
+  , AutfiltResult(..)
+  )
+
+import System.IO (readFile)
+import Control.Exception
+  ( IOException
+  , try)
+
+-- | Check if a hoa is a valid one according to spot
+checkValidHOA :: String -> IO (Either String Bool)
+checkValidHOA hoa = do
+  let input = defaultAutfiltInput {automaton = hoa}
+  res <- autfilt input
+  case res of
+    AutfiltSuccess _ -> return $ Right True
+    AutfiltNoMatch -> return $ Right False
+    AutfiltFailure _ -> return $ Right False
+    AutfiltException err -> return $ Left err
+
+
+wrap :: (String -> Either String b) -> HOA -> Either String b
+wrap f hoa = f (printHOA False hoa)
+
+wrapTransform :: (String -> Either String String) -> HOA -> Either String HOA
+wrapTransform f hoa = (wrap f hoa) >>= parse
+
+wrapFile :: (String -> Either String b) -> String -> IO (Either String b)
+wrapFile f path = do
+  content <- try $ readFile path
+  case content of
+    Left ex -> do
+      let err = show (ex :: IOException)
+      return $ Left err
+    Right content ->
+      return $ (parse content) >>= (wrap f)
+
+wrapFileTransform :: (String -> Either String String) -> String -> IO (Either String HOA)
+wrapFileTransform f path = do
+  res <- wrapFile f path
+  return $ res >>= parse
